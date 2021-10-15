2633bf7ced05df4c16396a5fe8d63e4442986fb2
carly-west <carly.west99@gmail.com>
2021-10-15 15:21:11 -0600

    Added str stuff

--- 05-jumper/jumper/game/board.py
+++ 05-jumper/jumper/game/board.py
@@ -10,7 +10,6 @@ class Board:
         print(f"This is the random word: {self.word_bank.rand_word}")
 
     def word_to_list(self):
-        print(f"{self.word_bank.rand_word}")
-        # word = self.word_bank.get_random_word()
-        # list_of_letters = list(word)
-        # print(list_of_letters)
+        word = str(self.word_bank.get_random_word())
+        list_of_letters = list(word)
+        print(list_of_letters)
