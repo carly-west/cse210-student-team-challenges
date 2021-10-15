777f3fcdef3933c574dd00e4a379ff0b29121476
carly-west <carly.west99@gmail.com>
2021-10-15 14:42:58 -0600

    c

--- 05-jumper/jumper/game/jumper.py
+++ 05-jumper/jumper/game/jumper.py
@@ -1,5 +1,23 @@
-from game.board import Board
+# from game.board import Board
 
 """
 Display and update jumping mans parachute
 """
+
+print("_ _ _ _ _")
+
+row1 = ("  ___  ")
+row2 = (" /___\ ")
+# row2a = ("|     | ")
+row3 = (" \   / ")
+row4 = ("  \ /  ")
+person = ("   0   \n  /|\  \n  / \  \n^^^^^^^")
+
+
+print(row1)
+print(row2)
+# print(row2a)
+print(row3)
+print(row4)
+
+print(person)
