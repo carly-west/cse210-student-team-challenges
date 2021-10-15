9a068622acde64768441062d30d41f675eef728a
RJSanchezR <san16071@byui.edu>
2021-10-15 13:07:53 -0600

    Some of basic structure completed

--- 05-jumper/jumper/game/director.py
+++ 05-jumper/jumper/game/director.py
@@ -1,5 +1,25 @@
-from game.console import console
-from game.print_board import Print_board
-"""
+from game.board import Board
+from game.word_bank import Word_Bank
 
-"""
+class Director:
+    def __init__(self):
+
+        self.play_again = True
+        self.board = Board()
+
+    def start_game(self):
+
+        while self.play_again:
+            self.get_inputs()
+
+    def get_inputs(self):
+
+        self.board.print()
+
+        again = input("Do you want to play again? [y/n] :")
+        if again.lower() == 'y':
+            self.play_again = True
+        elif again.lower() == 'n':
+            self.play_again = False
+        else:
+            again = ("That is not a valid input. Please enter 'y' for yes and 'n' for no")
