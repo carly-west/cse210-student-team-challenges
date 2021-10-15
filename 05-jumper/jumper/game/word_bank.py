93677366e09bb395318237282909d2a996dfb3dc
RJSanchezR <san16071@byui.edu>
2021-10-15 13:07:15 -0600

    Got it finally work

--- 05-jumper/jumper/game/word_bank.py
+++ 05-jumper/jumper/game/word_bank.py
@@ -1,36 +1,18 @@
 import random
 # from urllib.request import urlretrieve
 
-# to access this, just import and call Word_bank()
-
-
 class Word_Bank:
     def __init__(self):
-        self.rand_word = "random word"
+        self.rand_word = ""
+
+    def get_random_word(self):
+        words = ['amigo', 'sasquach', 'geronimo', 'butter']
+        self.rand_word = words[random.randint(0, 3)]
 
 
-words = ['amigo', 'sasquach', 'geronimo', 'butter']
-random_word = words[random.randint(0, 3)]
-print(random_word)
+'''This code is for the extra
 
-# class Word_Bank:
-#     def __init__(self):
-#         self.rand_word = "this is not a word"
-
-#     def get_random_word(self):
-#         words = ['test', 'congeniality', 'computer', 'hippotamus', 'amigo']
-#         randint = random.randint(0,4)
-#         self.rand_word = words[randint]
-
-
-# def main():
-#     rando = Word_Bank()
-#     rando.get_random_word()
-
-# main()
-
-
-'''def __init__(self):
+def __init__(self):
     self.url = 'https://www.mit.edu/~ecprice/wordlist.10000'
 
 def urlretrieve(url, words):
