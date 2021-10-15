570edfa93ea21421e2105db6c1017388694ffa19
carly-west <carly.west99@gmail.com>
2021-10-15 15:08:10 -0600

    asdf

--- 05-jumper/jumper/game/board.py
+++ 05-jumper/jumper/game/board.py
@@ -1,5 +1,6 @@
 from game.word_bank import Word_Bank
 
+
 class Board:
     def __init__(self):
         self.word_bank = Word_Bank()
@@ -7,3 +8,9 @@ class Board:
     def print(self):
         self.word_bank.get_random_word()
         print(f"This is the random word: {self.word_bank.rand_word}")
+
+    def word_to_list(self):
+        print(f"{self.word_bank.rand_word}")
+        # word = self.word_bank.get_random_word()
+        # list_of_letters = list(word)
+        # print(list_of_letters)
