0e2d1b9c40d3b7db98db923de9b7b9f698b7217f
carly-west <carly.west99@gmail.com>
2021-10-15 15:41:24 -0600

    hi

--- 05-jumper/jumper/game/board.py
+++ 05-jumper/jumper/game/board.py
@@ -5,11 +5,10 @@ class Board:
     def __init__(self):
         self.word_bank = Word_Bank()
 
-    def print(self):
-        self.word_bank.get_random_word()
-        print(f"This is the random word: {self.word_bank.rand_word}")
-
     def word_to_list(self):
         word = str(self.word_bank.get_random_word())
         list_of_letters = list(word)
-        print(list_of_letters)
+        return list_of_letters
+
+    def put_user_guess(self):
+        user_guess = input("Make a guess: ")
