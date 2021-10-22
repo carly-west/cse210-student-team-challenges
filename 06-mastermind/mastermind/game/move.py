1d08bb35ff5decb4c89b479b849d7c0bc9e23124
RJSanchezR <san16071@byui.edu>
2021-10-21 18:50:04 -0600

    added function to parse number

--- 06-mastermind/mastermind/game/move.py
+++ 06-mastermind/mastermind/game/move.py
@@ -2,7 +2,13 @@
 
 class Move:
     def __init__(self):
-        self
+        self.number_list = []
 
+    def number_to_list(self, number1):
+        number_to_string = str(number1)
+        self.number_list = number_to_string.split()
+        print(self.number_list)
+        return self.number_list
+    
     # def compare_guess(self, number, guess):
-    #     if guess == number:
+        # if guess == number:
