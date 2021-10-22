e541b03c54aaf9fb86fd8ecf083a2ba73e6b4470
RJSanchezR <san16071@byui.edu>
2021-10-21 18:55:38 -0600

    added player name function

--- 06-mastermind/mastermind/game/player.py
+++ 06-mastermind/mastermind/game/player.py
@@ -1,7 +1,13 @@
 '''create player'''
 
 class Player:
-    def __init__(self):
-        self.player = ""
-
+    def __init__(self, name):
+        self._name = name
+ 
+    def get_name(self):
+        """Returns the player's name.
+        Args:
+            self (Player): an instance of Player.
+        """
+        return self._name
 
